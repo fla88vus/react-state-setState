f0037624411178ff84358131b9e7231d5f351359
fla88vus <flaviacardosi@gmail.com>
2021-03-30 16:25:31 +0200

    Added all cases

--- src/Users.js
+++ src/Users.js
@@ -1,34 +1,103 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useEffect } from "react";
+
+import { v4 } from "uuid";
 
 const Users = () => {
-  const [user, setUser] = useState(["Pluto", "Pippo"]);
-  console.log(user);
+  const [user, setUser] = useState([
+    { id: v4(), name: "Pluto" },
+    { id: v4(), name: "Pippo" }
+  ]);
+
+  const [objs, setObjs] = useState({
+    p: {
+      id: v4(),
+      name: "pippo",
+      address: []
+    },
+    q: {
+      id: v4(),
+      name: "pluto"
+    }
+  });
+
+  const [newO, setNewO] = useState({ id: v4(), name: "ciao" });
+
+  const editObjSpread = () => {
+    // Removing
+    const { name, ...rest } = newO;
+    console.log(rest);
+    // setNewO(rest)
+
+    const updateObj = { ...rest, name: "pippo" };
+    console.log(updateObj);
+    // setNewO(updateObj)
+  };
+  // editObjSpread();
+
+  const editObjState = () => {
+    const test = Object.keys(objs).map(prop => {
+      return objs[prop];
+    });
+    console.log(test);
+  };
+  editObjState();
+
   const addUser = () => {
-    setUser([...user, "Paperino"]);
+    setUser([...user, { id: v4(), name: "Paperino" }]);
   };
 
-  const modifyUser = () => {
-    const [Pluto, ...other] = user;
-    console.log("other", other);
-    setUser(["Minnie", ...other]);
+  const modifyUser = id => {
+    const userPluto = user.reduce((acc, u) => {
+      if (u.id === id) {
+        return [...acc, { id, name: "Minnie" }];
+      } else {
+        return [...acc, u];
+      }
+    }, []);
+
+    setUser(userPluto);
   };
 
-  
+  const removeUser = id => {
+    const userFiltered = user.filter(elem => {
+      return elem.id !== id;
+    });
+    setUser(userFiltered);
+  };
+
+  const addSurnames = () => {
+    const userMap = user.map(elem => {
+      return {
+        ...elem,
+        surname: "surname-" + elem.name
+      };
+    });
+    setUser(userMap);
+
+    //setUser([...user, {}]);
+  };
+
   return (
     <>
       <div>
-        {user.map((u, index) => {
+        {user.map(u => {
           return (
-            <ul key={index}>
-              <li>{u}</li>
+            <ul key={u.id}>
+              <li>
+                {u.name} {u?.surname}
+              </li>
             </ul>
           );
         })}
+        <button onClick={addUser}>Aggiungi</button>
+        <br />
+        <br />
+        <button onClick={() => modifyUser(user[0].id)}>Modifica Pluto</button>
+        <br />
+        <br />
+        <button onClick={() => removeUser(user[1].id)}>Elimina Pippo</button>
+        <button onClick={addSurnames}>Aggiungi soprannomi</button>
       </div>
-      <button onClick={addUser}>Aggiungi</button>
-      <br />
-      <br />
-      <button onClick={modifyUser}>Modifica Pluto</button>
     </>
   );
 };
